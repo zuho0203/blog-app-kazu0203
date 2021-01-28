6ee85c77209d0d4ea78b7496d914c9e9c58f0b8b
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-01-29 06:14:50 +0900

    no message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
     def index
-      @article = Article.first
+      @articles = Article.all
+    end
+
+    def show
+      @article = Article.find(params[:id])
     end
    
    end
