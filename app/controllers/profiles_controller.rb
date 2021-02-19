acd6c145b667692b5ffd0a8e93f61c7a811f9bed
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-02-19 11:39:16 +0900

    no message

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,34 @@ class ProfilesController < ApplicationController
     end
 
     def edit
-        @profile = current_user.build_profile
+    #  if current_user.profile.present?
+    #     @profile = current_user.profile
+    #  else
+    #     @profile = current_user.build_profile
+    #  end
+    @profile = current_user.prepare_profile
+    end
+
+    def update
+        @profile = current_user.prepare_profile
+        @profile.assign_attributes(profile_params)
+        if @profile.save
+            redirect_to profile_path, notice: 'プロフィール更新！'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :birthday,
+            :subscribed
+
+        )
     end
 end
