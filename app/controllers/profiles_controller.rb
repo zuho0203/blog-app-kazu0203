59bc53e26337fef2315675ea5838c11f0f297817
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-02-25 05:39:48 +0900

    no message

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -32,7 +32,8 @@ class ProfilesController < ApplicationController
             :introduction,
             :gender,
             :birthday,
-            :subscribed
+            :subscribed,
+            :avatar
 
         )
     end
