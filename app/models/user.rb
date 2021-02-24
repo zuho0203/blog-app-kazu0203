0ed9cb0b82828e79a4fd86474c67be0706436363
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-02-25 06:03:13 +0900

    Likesテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :gender, :age, to: :profile, allow_nil: true
