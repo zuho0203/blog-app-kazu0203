d3565a2e6a7ee40fff9a134ef5443aee1548be75
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-02-25 10:10:11 +0900

    いいね解除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :articles do
     resources :comments, only: [:new, :create]
-    resource :like, only: [:create]
+    resource :like, only: [:create, :destroy]
   end
 
   resource :profile, only: [:show, :edit, :update]
