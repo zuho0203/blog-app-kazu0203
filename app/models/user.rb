09accdcf353044ec5d6eeb3d3ea6e7be7e8b1c03
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-02-25 16:38:51 +0900

    お気に入りの記事表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
 
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :favorites_articles, through: :likes, source: :article
 
   has_one :profile, dependent: :destroy
 
