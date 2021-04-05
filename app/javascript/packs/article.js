13c8feb920c86d711cd57de33a203f5aae42db65
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-04-05 12:01:05 +0900

    namespace,scope設定

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -32,7 +32,7 @@ document.addEventListener('turbolinks:load', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
 
-  axios.get(`/articles/${articleId}/comments`)
+  axios.get(`/api/articles/${articleId}/comments`)
     .then((response) => {
         const comments = response.data
         comments.forEach((comment) => {
@@ -47,7 +47,7 @@ handleCommentForm()
         if (!content) {
             window.alert('コメントを入力してください')
         } else {
-            axios.post(`/articles/${articleId}/comments`, {
+            axios.post(`/api/articles/${articleId}/comments`, {
                 comment: {content: content}
             })
             .then((res) => {
@@ -58,7 +58,7 @@ handleCommentForm()
         }
     })
 
-  axios.get(`/articles/${articleId}/like`)
+  axios.get(`/api/articles/${articleId}/like`)
   .then((response) => {
     const hasLiked = response.data.hasLiked
     handleHeartDisplay(hasLiked)
